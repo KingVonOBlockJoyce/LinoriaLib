1f05b02378c7c7e3c01f43248f7391093526f3e5
KingVonOBlockJoyce <oblockjoycesohiorizz@gmail.com>
2025-02-14 21:20:45 +0000

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -1868,7 +1868,7 @@ do
         });
 
         local ToggleLabel = Library:CreateLabel({
-            Size = UDim2.new(0, 216, 1, 0);
+            Size = UDim2.new(0, 290, 1, 0);
             Position = UDim2.new(1, 6, 0, 0);
             TextSize = 14;
             Text = Info.Text;
